c1a4dfe7df5424c5348ddf964813fd275979b7e1
Harini Saladi <harini@gurupanguji.com>
2013-11-05 22:54:01 -0800

    configured

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,13 +62,10 @@ UserWaitlist::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: ENV["DOMAIN_NAME"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    address: "smtp.mandrillapp.com",
+    port: 25,
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
   }
 
 
