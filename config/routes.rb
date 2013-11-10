0fc41417e6464d4c8bae160818ea976d9cd5d4fe
Harini Saladi <harini@gurupanguji.com>
2013-11-10 15:19:37 -0800

    allow user to create password after invitation

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,14 @@
 UserWaitlist::Application.routes.draw do
   authenticated :user do
-    root :to => 'home#index' 
+    root :to => 'home#index'
   end
-  
   devise_scope :user do
     root :to => "devise/registrations#new"
-    match '/user/confirmation' => 'confirmations#update', :via => :put, :as =>
- :update_user_confirmation
+    match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
-
-  devise_for :users, :controllers => { :registrations => "registrations" }
+  devise_for :users, :controllers => { :registrations => "registrations", :confirmations => "confirmations" }
   match 'users/bulk_invite/:quantity' => 'users#bulk_invite', :via => :get, :as => :bulk_invite
-
   resources :users do
     get 'invite', :on => :member
   end
-  
 end
