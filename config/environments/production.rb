c923d77dba7f02d09ff758b6c747b7d734713d68
Harini Saladi <harini@gurupanguji.com>
2013-11-10 23:38:02 -0800

    update production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ UserWaitlist::Application.configure do
   }
 
 
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'http://safe-oasis-9185.herokuapp.com/' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
